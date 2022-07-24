5726006dcebcbbf04754595b3ea508dec064a259
CharmeeMahesh <b.maheshbandaru@gmail.com>
2022-07-24 12:31:13 +0530

    m11 method got added

--- Helloworld.java
+++ Helloworld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World!");
     }
+
+    public static void m11(String abc) {
+        System.out.println("Hello World!");
+    }
 }
